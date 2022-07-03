94193512ac04686768fbb3df847b4272afc0d577
unknown <2034457546@qq.com>
2022-07-03 21:56:28 +0800

    Site updated: 2022-07-03 21:56:24

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "4A86E8",
+      "tmpSize": "16",
+      "cityColor": "FF9900",
+      "citySize": "16",
+      "aqiColor": "4A86E8",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "0px 0px 0px 0px",
+      "shadow": "0",
+      "language": "auto",
+      "fixed": "true",
+      "vertical": "center",
+      "horizontal": "center",
+      "left": "200",
+      "top": "20",
+      "key": "2807d8ac960a4644981b8a234767f3f3"
+    }
+  }
+  
